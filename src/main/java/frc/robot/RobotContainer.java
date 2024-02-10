0bd0c0a808731d28a9aba0ce9ab3283096c17e0f
Humans4501 <nrsrobotics@gmail.com>
2024-02-09 16:31:10 -0800

    Bind sysid commands to joysticks

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,46 +19,57 @@ import frc.robot.subsystems.Shooter;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final Shooter sShooter = new Shooter();
+	// The robot's subsystems and commands are defined here...
+	private final Shooter sShooter = new Shooter();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandJoystick mJoystick = new CommandJoystick(OperatorConstants.kDriverControllerPort);
+	// Replace with CommandPS4Controller or CommandJoystick if needed
+	private final CommandJoystick mJoystick = new CommandJoystick(OperatorConstants.kDriverControllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-  }
+	/** The container for the robot. Contains subsystems, OI devices, and commands. */
+	public RobotContainer() {
+		// Configure the trigger bindings
+		this.configureBindings();
+	}
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    // new Trigger(m_exampleSubsystem::exampleCondition).onTrue(new ExampleCommand(m_exampleSubsystem));
+	/**
+	 * Use this method to define your trigger->command mappings. Triggers can be created via the
+	 * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+	 * predicate, or via the named factories in {@link
+	 * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+	 * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+	 * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+	 * joysticks}.
+	 */
+	private void configureBindings() {
+		// Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+		// new Trigger(m_exampleSubsystem::exampleCondition).onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-    
-    this.mJoystick.button(1).whileTrue(this.sShooter.cmdShoot());
-  }
+		// Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+		// cancelling on release.
+		// m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+		
+		this.mJoystick.button(1).whileTrue(this.sShooter.cmdShoot());
+		this.mJoystick.button(2)
+			.whileTrue(
+				this.sShooter.sysidTopQuasi()
+					.alongWith(this.sShooter.sysidBtmQuasi())
+			);
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    // return Autos.exampleAuto(m_exampleSubsystem);
-    return new PrintCommand("No auto :(");
-  }
+		this.mJoystick.button(3)
+			.whileTrue(
+				this.sShooter.sysidTopDynamic()
+					.alongWith(this.sShooter.sysidBtmDynamic())
+			);
+	}
+
+	/**
+	 * Use this to pass the autonomous command to the main {@link Robot} class.
+	 *
+	 * @return the command to run in autonomous
+	 */
+	public Command getAutonomousCommand() {
+		// An example command will be run in autonomous
+		// return Autos.exampleAuto(m_exampleSubsystem);
+		return new PrintCommand("No auto :(");
+	}
 }
