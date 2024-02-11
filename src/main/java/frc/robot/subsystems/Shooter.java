0a3863f8d4bac9c403c8c738ba5ea00db2a67605
Humans4501 <nrsrobotics@gmail.com>
2024-02-11 15:04:58 -0800

    set factors

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,49 +2,77 @@ package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
 
+import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
-//4501 2/7/2024 yadabadoooooo
-import edu.wpi.first.wpilibj.Timer;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.SparkPIDController.ArbFFUnits;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
+/**
+ * Shooter subsystem. PIDF stuff is in rad/s
+ */
 public class Shooter extends SubsystemBase {
-	private final int kTopShooterCanID = 32;
-	private final int kBottomShooterCanID = 33;
-	private final int kFeederCanID = 31;
+	private static final int kTopShooterCanID = 32;
+	private static final int kBottomShooterCanID = 33;
+	private static final int kFeederCanID = 31;
+
+	private static final double kPosEncFac = 2.0 * Math.PI;
+	private static final double kVelEncFac = (2.0 * Math.PI) / 60.0;
+
+	// Our gainz
+	private static final double kSff = 0.0;
+	private static final double kVff = 0.12928;
+	private static final double kAff = 0.10706;
+	private static final double kPpid = 0.00018357;
+	private static final double kDpid = 0.0;
 
 	//SparkMax controllers
-	private final CANSparkMax mShooterTop = new CANSparkMax(this.kTopShooterCanID, MotorType.kBrushless);
-	private final CANSparkMax mShooterBottom = new CANSparkMax(this.kBottomShooterCanID, MotorType.kBrushless);
-	private final CANSparkMax mFeedMotor = new CANSparkMax(this.kFeederCanID, MotorType.kBrushless);
+	private final CANSparkMax mShooterTop = new CANSparkMax(Shooter.kTopShooterCanID, MotorType.kBrushless);
+	private final RelativeEncoder mTopEncoder;
+	private final CANSparkMax mShooterBottom = new CANSparkMax(Shooter.kBottomShooterCanID, MotorType.kBrushless);
+	private final CANSparkMax mFeedMotor = new CANSparkMax(Shooter.kFeederCanID, MotorType.kBrushless);
+
+	private final SimpleMotorFeedforward mTopFeedf = new SimpleMotorFeedforward(kSff, kVff, kAff);
+	private final SparkPIDController mTopPid;
 
 	private static final String kMotorSpeedName = "Motor Speed";
 	private static final String kFeedSpeedName = "Feed Speed";
 	private static final String kSpinupTimeName = "Spinup Time";
-	private static final String kFeedTimeName = "Feed Time";
 	private static final String kStatusName = "Shooter Status";
-
-	private final Timer mTimer = new Timer();
+	private static final String kVelocityName = "Shooter Velocity";
 
 	public Shooter() {
 		this.mShooterBottom.follow(mShooterTop, true);
-		//returns motor speed and feed speed
+
+		this.mTopEncoder = this.mShooterTop.getEncoder();
+		this.mTopEncoder.setPositionConversionFactor(kPosEncFac);
+		this.mTopEncoder.setVelocityConversionFactor(kVelEncFac);
+
+		this.mTopPid = this.mShooterTop.getPIDController();
+		this.mTopPid.setP(kPpid);
+		this.mTopPid.setI(0.0);
+		this.mTopPid.setD(kDpid);
+
 		SmartDashboard.setDefaultNumber(kMotorSpeedName, 0);
 		SmartDashboard.setDefaultNumber(kFeedSpeedName, 0);
 		SmartDashboard.setDefaultNumber(kSpinupTimeName, 2.0);
-		SmartDashboard.setDefaultNumber(kFeedTimeName, 1.0);
 		SmartDashboard.setDefaultString(kStatusName, "");
+		SmartDashboard.setDefaultNumber(kVelocityName, 0.0);
 
 		this.setDefaultCommand(this.run(() -> {
 			this.mShooterTop.stopMotor();
 			this.mFeedMotor.stopMotor();
-			this.mTimer.stop();
 			SmartDashboard.putString(kStatusName, "Stopped shooting");
 		}));
 
@@ -65,17 +93,25 @@ public class Shooter extends SubsystemBase {
 		final Command shootCmd = new SequentialCommandGroup(
 			this.runOnce(() -> {
 				//Restarts timer and sets the speed
-				this.mShooterTop.set(SmartDashboard.getNumber(kMotorSpeedName, 0));
-				this.mTimer.restart();
-				SmartDashboard.putString(kStatusName, "Spinning up...");
 			}),
+			new FunctionalCommand(
+				() -> {},
+				() -> {
+					double setpoint = SmartDashboard.getNumber(kMotorSpeedName, 0.0);
+					SmartDashboard.putString(kStatusName, "Spinning up...");
+					this.mTopPid.setReference(setpoint, ControlType.kVelocity, 0, this.mTopFeedf.calculate(setpoint), ArbFFUnits.kVoltage);
+					SmartDashboard.putNumber(kVelocityName, this.mTopEncoder.getVelocity());
+				},
+				(Boolean inter) -> {},
+				() -> false,
+				this
+			),
 			this.deferredWaitCommand(() -> SmartDashboard.getNumber(kSpinupTimeName, 2.0)),
 			//Sets speed of feed motor
 			this.runOnce(() -> {
 				this.mFeedMotor.set(SmartDashboard.getNumber(kFeedSpeedName, 0));
 				SmartDashboard.putString(kStatusName, "Feeding");
 			}),
-			this.deferredWaitCommand(() -> SmartDashboard.getNumber(kFeedTimeName, 1.0)),
 			new RepeatCommand(new InstantCommand())
 		);
 
